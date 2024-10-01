8de7d2592addc0f0f83c505e643c5de8bf74c1cc
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-01 18:31:28 +0100

    update with only Route::resource

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+// Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::get('/create', function () {
-    return view('create');
-});
+// Route::get('/create', function () {
+//     return view('create');
+// });
 
 Route::resource('/tasks', TaskController::class);
