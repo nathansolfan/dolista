59eb5c20adbae2272eb18e82fa682b9d768dedaa
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-05 16:05:44 +0100

    style the create form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,17 +1,29 @@
 <x-layout>
 
-    <h1 class="flex flex-col items-center">Create me</h1>
-    <form action=" {{ route('tasks.store')}}" method="POST">
+    <div class="flex flex-col items-center">
+    <h1 class="text-3xl font-semibold mb-3">Create me</h1>
+    <section class="shadow-md rounded-lg p-4 mb-4">
+    <form action=" {{ route('tasks.store')}}" method="POST" class="space-y-4">
         @csrf
         <div>
-            <label for="">Title</label>
-            <input type="text" name="title" id="title">
+            <label for="">Title:</label>
+            <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Enter title"
+            class="mt-1 py-2 px-3 border border-gray-300 rounded-md shadow-sm @error('title') border-red-500 @else border-gray-300 @enderror
+"
+            >
         </div>
-
         <div>
-            <label for="">Description</label>
-            <input type="text" name="description" id="description">
+            <label for="">Description:</label>
+            <input type="text" name="description" id="description" placeholder="Enter description" class="mt-1 py-2 px-3 border border-gray-300 rounded-md shadow-sm">
         </div>
-        <button type="input">Click me</button>
+        <button type="input"
+        class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >Click me</button>
     </form>
+</section>
+</div>
 </x-layout>
